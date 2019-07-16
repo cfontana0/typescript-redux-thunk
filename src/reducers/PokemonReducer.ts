dddd44b5e11add429f977ad43d751658cc7c76fa
Carlos Fontana <>
2019-07-16 15:16:49 -0300

    Typescript first implementation

---
+++ src/reducers/PokemonReducer.ts
@@ -0,0 +1,38 @@
+import {
+	PokemonDispatchTypes,
+	PokemonType,
+	POKEMON_FAIL,
+	POKEMON_LOADING,
+	POKEMON_SUCCESS
+} from '../actions/PokemonActionTypes';
+
+interface DefaultStateI {
+	loading: boolean;
+	pokemon?: PokemonType;
+}
+
+const defaultState: DefaultStateI = {
+	loading: false
+};
+
+const pokemonReducer = (state: DefaultStateI = defaultState, action: PokemonDispatchTypes): DefaultStateI => {
+	switch (action.type) {
+		case POKEMON_FAIL:
+			return {
+				loading: false
+			};
+		case POKEMON_LOADING:
+			return {
+				loading: true
+			};
+		case POKEMON_SUCCESS:
+			return {
+				loading: false,
+				pokemon: action.payload
+			};
+		default:
+			return state;
+	}
+};
+
+export default pokemonReducer;
